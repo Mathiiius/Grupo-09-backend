63004b0ed6a305534370cd98ea396bedeaf1e393
Mathiiius <angelbritos621@gmail.com>
2024-07-01 05:37:01 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
+});
+
+
+/*const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'movies_db',
     port: 3306
   });
-  
+  */
   db.connect(err => {
     if (err) {
       console.error('Error connecting to the database:', err);
